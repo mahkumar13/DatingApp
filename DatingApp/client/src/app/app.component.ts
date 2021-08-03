e7b44c14583b798cc43f0dcac34ca69a9998b715
Mahendra Kumar <mahkumar13@publicisgroupe.net>
2021-08-03 22:47:55 +0530

    end of section 5

--- DatingApp/client/src/app/app.component.ts
+++ DatingApp/client/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { getMissingNgModuleMetadataErrorData } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
+import { User } from './_Models/user';
+import { AccountService } from './_Services/account.service';
 
 @Component({
   selector: 'app-root',
@@ -11,11 +13,19 @@ export class AppComponent  implements OnInit{
   title = 'The Dating App';
   users :any;
 
-  constructor(private http:HttpClient){}
+  constructor(private http:HttpClient,private accountService:AccountService){}
   ngOnInit(): void {
     this.getUsers();
+    this.setCurrentUser();
     
   }
+setCurrentUser()
+{
+  //const user:User=JSON.parse(localStorage.getItem('user'));
+  //this.accountService.setCurrentUser(user);
+}
+
+
     getUsers(){
       this.http.get('https://localhost:5001/api/users').subscribe(response =>{
       this.users =response;
